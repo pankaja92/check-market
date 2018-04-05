e05b0809d061e8b862f3c733a29a6959ad92c8fa
pankaja92 <pankajanc@gmail.com>
2018-04-05 18:23:12 +0530

    Fix hosting issues

--- index.js
+++ index.js
@@ -7,4 +7,12 @@ const app = express();
 
 app.use('/', routes);
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    const path = require('path');
+    app.get("*", (req,res) =>  {
+      res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
+  }
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
